8fc9f92ecaf5bf1044eb240c3ba3c55d64a8951e
David Igandan <davidigandan@gmail.com>
2026-02-20 19:22:57 +0000

    code for multithreading

--- is_prime.txt.py
+++ is_prime.txt.py
@@ -72,6 +72,7 @@ import concurrent.futures
 from timeit import default_timer as timer
 import multiprocessing
 from math import sqrt
+import time
 
 def is_prime(x):
     if x<2:
@@ -92,30 +93,33 @@ def is_prime(x):
 
 
 def concurrent_solve(n_workers):
-    print("Number of workers: %i." % n_workers )
-
     result = []
+    
+    with concurrent.futures.ProcessPoolExecutor(max_workers=n_workers) as executor:
+        # sequential phase
+        print("Solving with %i worker(s)." % n_workers )
+        start =timer()
+        futures = [executor.submit(is_prime, i) for i in input]
+        completed_futures = concurrent.futures.as_completed(futures)
+        sub_start = timer()
+        # concurrent phase
+        
+        for i,future in enumerate(completed_futures):
+            if future.result():
+                result.append(future.result())
 
-    if __name__ == '__main__':
-        with concurrent.futures.ProcessPoolExecutor(max_workers=n_workers) as executor:
-            # concurrent phase
-            start =timer()
-            futures = [executor.submit(is_prime, i) for i in input]
-            completed_futures = concurrent.futures.as_completed(futures)
-            stop = timer()
-            # sequential phase
-            seq_start = timer()
+        sub_duration = timer() - sub_start
+        duration = timer() - start
+        print("Sub took ", sub_duration, "to complete concurrent phase")
+        print("Took", duration - sub_duration, "to complete sequential pahse")
+        print("Took ", duration, "to complete sequential+concurrent phase")
 
-            for i,future in enumerate(completed_futures):
-                if future.result():
-                    result.append(future.result())
+    # time.sleep(15)
 
-            seq_end = timer()
-        print("It took ", stop -start, "to complete concurrent phase")
-        print("it took ", seq_end-seq_start, "to complete sequential phase")
 
-input = [i for i in range(10 ** 13, 10 **13 + 1000)]
+if __name__ == "__main__":
+    input = [i for i in range(10 ** 13, 10 **13 + 10000)]
 
-for n_workers in range(1, multiprocessing.cpu_count()+1):
-    concurrent_solve(n_workers)
-    print("_" * 20)
+    for n_workers in range(1, multiprocessing.cpu_count()+1):
+        concurrent_solve(n_workers)
+        print("_" * 20)
